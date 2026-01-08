afa07f307942a4b39e809149dada771abab5c740
DAVMONT08 <davide.montella@marconibari.edu.it>
2026-01-08 00:55:04 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,100 @@
 #include <iostream>
 using namespace std;
 
+void ordinavettore(int n, int v[]);
+
+void riempivettore(int n, int v[], bool flag);
+
+void visualizza(int n, int v[]);
+
+void vsualizzainverso(int n, int v[]);
+
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int n;
+    bool flag;
+
+    cout << "quanti numeri generare" << endl;
+    cin >> n;
+    int p[n], d[n];
+
+    flag = true;
+    riempivettore(n, p, flag);
+    flag = false;
+    riempivettore(n, d, flag);
+    ordinavettore(n, p);
+    ordinavettore(n, d);
+    cout << "vettore pari ordinato in ordine crescente" << endl;
+    visualizza(n, p);
+    cout << "vettore dispari ordinato in ordine crescente" << endl;
+    visualizza(n, d);
+    cout << "vettore dispari ordinato in ordine decrescente" << endl;
+    visualizzainverso(n, d);
+    return 0;
+}
+
+void ordinavettore(int n, int v[]) {
+    int i, j, t;
+    bool scambio;
+
+    i = 0;
+    scambio = true;
+    while (j <= n - 1 && scambio) {
+        scambio = false;
+        j = 0;
+        while (j <= n - 2 - i) {
+            if (v[j] > v[j + 1]) {
+                scambio = true;
+                t = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = t;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+}
+
+void riempivettore(int n, int v[], bool flag) {
+    int i, x;
+
+    i = 0;
+    if (flag == true) {
+        while (i < n) {
+            x = 1 + rand() % 100;
+            if (x % 2 == 0) {
+                v[i] = x;
+                i = i + 1;
+            }
+        }
+    } else {
+        while (i < n) {
+            x = 1 + rand() % 100;
+            if (x % 2 == 0) {
+            } else {
+                v[i] = x;
+                i = i + 1;
+            }
+        }
+    }
+}
+
+void visualizza(int n, int v[]) {
+    int i;
+
+    i = 0;
+    while (i < n) {
+        cout << v[i] << endl;
+        i = i + 1;
+    }
+}
+
+void vsualizzainverso(int n, int v[]) {
+    double i;
+
+    while (i >= 0) {
+        cout << v[i] << endl;
+        i = i - 1;
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
